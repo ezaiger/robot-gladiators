3918f15887935c539380913ae594953218494910
EZaiger <elisabeth.zaiger@oit.edu>
2022-05-17 00:32:47 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,8 @@
-var playerName = window.prompt("What is your robot's name?"); 
+// Player name
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -8,41 +10,70 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// Fight function
+var fight = function () {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!"); 
+    window.alert("Welcome to Robot Gladiators!");
 };
 
-// Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-enemyHealth = enemyHealth - playerAttack;
+// Ask player if they'd like to fight or run
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-// Log a resulting message to the console so we know that it worked.
-console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-);
+// If player chooses to fight, fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
 
-// Check enemy's health
-if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!")
-}
-else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.")
-}
+    // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
 
-// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-// Log a resulting message to the console so we know that it worked.
-console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-);
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!")
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.")
+    }
 
-// Check player's health
-if (playerHealth <=0) {
-    window.alert(playerName + " has died!")
-}
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.")
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!")
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.")
+    }
+
+    // If player chooses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+    // Confirm the player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // If yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // Subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+    }
+
+    // if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
+
+} else {
+    window.alert("You need to try a valid option. Try again!");
 }
 
 fight();
@@ -78,4 +109,9 @@ var fight = function()  is when we create a function by assigning it to a variab
 
 ===                     allows you to check if two values are directly equal to one another
 
+if                      anything put between the parentheses of an 'if' statement will result in true or false value
+
+||                      the "or" operator; tells the program to execute that code as long as at lease one of the condition checks results in true. If both are false, move on.
+
+window.confirm()        requires a yes/no answer
 */
