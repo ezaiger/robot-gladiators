a3492e28aae8b1fa3fecb93cbd9aae97d5515e6a
EZaiger <elisabeth.zaiger@oit.edu>
2022-05-17 00:01:44 -0700

    player decision additions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,51 @@
-
-
 var playerName = window.prompt("What is your robot's name?"); 
-    console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(10 + 10);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("Our robot's name is " + playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight () {
-    window.alert("The fight has begun!"); 
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!"); 
+};
+
+// Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// Check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!")
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.")
 }
 
-//fight();
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+// Check player's health
+if (playerHealth <=0) {
+    window.alert(playerName + " has died!")
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.")
+}
+
+fight();
 
 
 
@@ -39,5 +73,9 @@ fight();                calls on the above function with the same name
 console.log             allows developer to test code without interrupting page with alert
 
 +                       allows you to include a variable with a string (text)                        
+
+var fight = function()  is when we create a function by assigning it to a variable
+
+===                     allows you to check if two values are directly equal to one another
 
 */
